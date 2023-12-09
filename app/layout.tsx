58758f3d8dba78bea3db537b77c2acd7b736329c
Uplix <119914156+Uplix@users.noreply.github.com>
2023-12-09 09:32:41 -0800

    this is the start of the login page need to implement next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,7 +18,8 @@ export default function RootLayout({
 }) {
     return (
         <html lang='en'>
-            <Navbar />
+            
+            {/* <Navbar /> */}
             <body className={inter.className}>{children}</body>
         </html>
     )
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
         <main className='flex min-h-screen flex-col items-center space-y-8 p-24'>
             <h1 className='bg-gradient-to-br from-indigo-500 from-20% via-sky-500 via-40% to-emerald-500 text-2xl px-4 py-3 rounded-xl'>This is the <strong>Main</strong> Page</h1>
             <div className='flex flex-col space-y-2'>
-                <Link href={"/loggedin/student"}>
+                <Link href={"/login/studentLogin"}>
                     <h2 className='bg-gradient-to-br from-fuchsia-600 from-20% to-red-400 to-80% text-md p-2 rounded-xl hover:p-4 hover:text-xl transition-all'>Go to Student View</h2>
                 </Link>
-                <Link href={"/loggedin/staff"}>
+                <Link href={"/loggedin/staffLogin"}>
                     <h2 className='bg-gradient-to-br from-fuchsia-600 from-20% to-red-400 to-80% text-md p-2 rounded-xl hover:p-4 hover:text-xl transition-all'>Go to Staff View</h2>
                 </Link>
             </div>
