47c88eb353199f78cde9aadd1c9d8787fa0561ef
Uplix <119914156+Uplix@users.noreply.github.com>
2024-05-01 22:07:54 -0700

    just doing this to change the .env on the website

--- app/go/team/page.tsx
+++ app/go/team/page.tsx
@@ -3,7 +3,9 @@ import UnderConstruction from "@/components/underConstruction"
 export default function Team(){
     return(
         <div className='flex min-h-screen flex-col items-center space-y-8 p-24'>
-            <UnderConstruction/>
+            {/* <UnderConstruction/> */}
+            <h1 className="text-5xl md:text-6xl font-sans">Join us</h1>
+            <h4 className="text-xl opacity-80 font-light ">We need more people to help us make peer tutoring better</h4>
         </div>
     )
 }
